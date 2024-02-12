fca7bb3f59bb985e95966cb78650c085c05d77d5
gpaul <40648068+gpaul-faldin@users.noreply.github.com>
2024-02-12 01:50:54 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-import express, {json} from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config();
 import Mainrouter from './routes/index.route'
-import FileHandlingClass from './class/fileHandling.class'
-import FileInfo from './models/fileInfo.model'
-import axios from 'axios';
+
 require('./mongoConnection');
 
 (async () => {
-
-  if (!process.env.DISCORD_TOKEN) throw new Error('Discord token is missing')
-
   const app = express()
-  //app.use(json())
 
   app.get("/", (req, res) => {
     res.status(418).send('This is a cloud service provider using discord')
@@ -23,27 +17,6 @@ require('./mongoConnection');
   app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
   })
-
-  // const testNAME = 'WebStorm.exe'
-
-  // const NewClass = new FileHandlingClass(process.env.DISCORD_TOKEN)
-
-  // if (await FileInfo.findOne({ filename: testNAME }))
-  //   return console.log('File already exists')
-
-  // const nfo = await NewClass.uploadFile(`./files/${testNAME}`)
-
-  // const fileInfo = new FileInfo({
-  //   filename: nfo.filename,
-  //   part: nfo.part,
-  //   size: nfo.size,
-  //   files: nfo.files
-  // })
-  // await fileInfo.save()
-
-  // const buffer = await NewClass.retrieveFileByName(testNAME)
-  // if (!buffer) return console.log('No buffer found')
-  // fs.writeFileSync(`./${testNAME}`, buffer, 'binary')
 
 })();
 
